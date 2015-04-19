5d9441447aa889eb930bf54c50863e0c4ac0d3d9
tziporaziegler <tziporaziegler@gmail.com>
2015-04-19 14:43:08 -0400

    finish commit

---
+++ src/airHockey/ClientWorld.java
@@ -0,0 +1,19 @@
+package airHockey;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class ClientWorld extends World {
+	private static final long serialVersionUID = 1L;
+
+	public ClientWorld(String serverAddress) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+		setLocationRelativeTo(null);
+		socket = new Socket(serverAddress, 3769);
+		// 1 = server second
+		setUp(1);
+	}
+
+}
